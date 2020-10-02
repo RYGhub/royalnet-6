8b28418867ba41c5c878b4f61b5a30272ecf07b4
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-10-02 03:17:13 +0200

    ✨ Create first prototype of the Alchemist module

---
+++ royalnet/alchemist/__init__.py
@@ -0,0 +1,21 @@
+from typing import *
+import sqlalchemy as sa
+
+
+__all__ = (
+    "Alchemist",
+)
+
+
+class Alchemist:
+    """The Alchemist module connects to a relational database with SQLAlchemy."""
+
+    def __init__(self,
+                 engine_args: Iterable[Any],
+                 engine_kwargs: Mapping[str, Any]):
+        self.engine: sa.engine.Engine = sa.create_engine(*engine_args, **engine_kwargs)
+
+    def add_metadata(self, metadata: sa.MetaData):
+        """Bind a MetaData object to the engine, and create all tables linked with it."""
+        metadata.bind = self.engine
+        metadata.create_all()
