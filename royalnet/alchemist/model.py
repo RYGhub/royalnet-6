ac51f3268ef1c6024745918ae466aa68aff5a7ce
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-11-06 17:57:37 +0100

    🐞 Remove cache as it didn't work as expected

--- royalnet/alchemist/model.py
+++ royalnet/alchemist/model.py
@@ -6,7 +6,6 @@ class PyModel:
     """A mixin that can be added to a declared class to generate a Pydantic model from it."""
 
     @classmethod
-    @functools.lru_cache(1)
     def pydantic(cls):
         return pydantic_sqlalchemy.sqlalchemy_to_pydantic(cls)
 
