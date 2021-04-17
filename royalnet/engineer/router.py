7cb296cea18e69cbdbcff77ae66aaf35c03cae3f
Stefano Pigozzi <me@steffo.eu>
2021-04-17 03:53:27 +0200

    🐛 Fixes #10

--- royalnet/engineer/router.py
+++ royalnet/engineer/router.py
@@ -82,7 +82,19 @@ class Router(c.Conversation, metaclass=abc.ABCMeta):
                 if match:
                     conversation = self.by_pattern[pattern]
                     log.debug(f"Matched with {pattern!r}, running conversation {conversation}")
-                    await conversation(_sentry=_sentry, **kwargs, _conv=conversation, _router=self)
+                    await conversation(
+                        **kwargs,
+                        _sentry=_sentry,
+                        _conv=conversation,
+                        _msg=msg,
+                        _text=text,
+                        _router=self,
+                    )
                     return
             else:
                 log.debug("No matches found")
+
+
+__all__ = (
+    "Router",
+)
